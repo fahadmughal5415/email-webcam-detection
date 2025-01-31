394110f725c0ff50d3cca1e73c32a7d64c43b78a
Programming Point <fahadmughal5415@gmail.com>
2025-01-31 15:02:17 +0500

    Implement motion detection in video feed

--- main.py
+++ main.py
@@ -3,10 +3,22 @@ import time
 
 video = cv2.VideoCapture(0)
 time.sleep(1)
-
+first_frame = None
 while True:
     check, frame = video.read()
-    cv2.imshow("My Video", frame)
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray_frame_gau = cv2.GaussianBlur(gray_frame, (21, 21), 0)
+
+    if first_frame is None:
+        first_frame = gray_frame_gau
+        continue
+    diff_frame = cv2.absdiff(first_frame, gray_frame_gau)
+
+    thresh_frame = cv2.threshold(diff_frame, 60, 255, cv2.THRESH_BINARY)[1]
+
+    cv2.dilate(thresh_frame, None, iterations=2)
+
+    cv2.imshow("My Video", thresh_frame)
 
     key = cv2.waitKey(1)
 
