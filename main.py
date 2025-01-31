4a5c9a0da2d3b416684abedc8a30b886d54dd3b7
Programming Point <fahadmughal5415@gmail.com>
2025-01-31 15:50:49 +0500

    Add email notification on motion detection

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import cv2
 import time
+from emailing import send_email
 
 video = cv2.VideoCapture(0)
 time.sleep(1)
 first_frame = None
+status_list = []
 while True:
+    status = 0
     check, frame = video.read()
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     gray_frame_gau = cv2.GaussianBlur(gray_frame, (21, 21), 0)
@@ -16,9 +19,30 @@ while True:
 
     thresh_frame = cv2.threshold(diff_frame, 60, 255, cv2.THRESH_BINARY)[1]
 
-    cv2.dilate(thresh_frame, None, iterations=2)
+    dil_frame = cv2.dilate(thresh_frame, None, iterations=2)
 
-    cv2.imshow("My Video", thresh_frame)
+    cv2.imshow("My Video", dil_frame)
+
+    contours, check = cv2.findContours(dil_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    for contour in contours:
+        if cv2.contourArea(contour) < 10000:
+            continue
+        x, y, w, h = cv2.boundingRect(contour)
+
+        rectangle = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
+
+        if rectangle.any():
+            status = 1
+
+    status_list.append(status)
+    status_list = status_list[-2:]
+    
+    if status_list[0] == 1 and status_list[1] == 0:
+        send_email()
+
+
+    cv2.imshow("Tracking", frame)
 
     key = cv2.waitKey(1)
 
