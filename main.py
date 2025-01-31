b59f2bf5a91f5b12ee300734b40f1330724bb95a
Programming Point <fahadmughal5415@gmail.com>
2025-01-31 17:43:49 +0500

    Capture and save images when motion is detected

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import cv2
 import time
 from emailing import send_email
+import glob
 
 video = cv2.VideoCapture(0)
 time.sleep(1)
 first_frame = None
 status_list = []
+count = 1
+all_images = []
 while True:
     status = 0
     check, frame = video.read()
@@ -26,7 +29,7 @@ while True:
     contours, check = cv2.findContours(dil_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     for contour in contours:
-        if cv2.contourArea(contour) < 10000:
+        if cv2.contourArea(contour) < 5000:
             continue
         x, y, w, h = cv2.boundingRect(contour)
 
@@ -34,12 +37,19 @@ while True:
 
         if rectangle.any():
             status = 1
+            cv2.imwrite(f"images/{count}.png", frame)
+            count += 1
+            all_images = glob.glob("images/*.png")
+            index = int(len(all_images) / 2)
+            image_with_object = all_images[index]
+
 
     status_list.append(status)
     status_list = status_list[-2:]
     
     if status_list[0] == 1 and status_list[1] == 0:
         send_email()
+        
 
 
     cv2.imshow("Tracking", frame)
