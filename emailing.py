c32935373941bd983f68d27eff8d39cf888339a8
Programming Point <fahadmughal5415@gmail.com>
2025-01-31 18:44:07 +0500

    Add email functionality to send images when a new customer is detected

--- emailing.py
+++ emailing.py
@@ -1,2 +1,30 @@
-def send_email():
-    print("Email sent")
+import imghdr
+from email.message import EmailMessage
+import smtplib
+import os
+
+
+SENDER = "fahadmughal5415@gmail.com"
+RECEIVER = "fahadmughal5415@gmail.com"
+PASSWORD = os.getenv("PASSWORD")
+HOST = "smtp.gmail.com"
+PORT = 587
+def send_email(image_path):
+    email_message = EmailMessage()
+    email_message["Subject"] = "New Customer Showed Up!"
+    email_message.set_content = "Hey, we just saw a new customer"
+
+    with open(image_path, "rb") as file:
+        content = file.read()
+
+    email_message.add_attachment(content, maintype="image", subtype=imghdr.what(None, content))
+
+    gmail = smtplib.SMTP(HOST, PORT)
+    gmail.ehlo()
+    gmail.starttls()
+    gmail.login(SENDER, PASSWORD)
+    gmail.sendmail(SENDER, RECEIVER, email_message.as_string())
+    gmail.quit()
+
+if __name__ == "__main__":
+    send_email("images/1.jpg")
